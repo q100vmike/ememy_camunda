93bce4fd56947e4a281491668c5ae808a675e0fb
q100vmike <melkin@mail.ru>
2024-07-23 15:36:17 +0300

    complite

--- src/main/java/ru/demo/enemy_camunda/PrepareToBattle.java
+++ src/main/java/ru/demo/enemy_camunda/PrepareToBattle.java
@@ -8,11 +8,21 @@ import org.springframework.stereotype.Component;
 public class PrepareToBattle implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        int warriors = (int) delegateExecution.getVariable("warriors");
         int enemyWarriors = (int) (Math.random() * 100);
         String battleStatus = "Undefined";
+        boolean isWin = false;
+
+        if (warriors - enemyWarriors > 0) {
+            isWin = true;
+            battleStatus = "Victoty!";
+        } else {
+            battleStatus = "Fail :(";
+        }
 
         delegateExecution.setVariable("battleStatus", battleStatus);
         delegateExecution.setVariable("enemyWarriors", enemyWarriors);
-
+        delegateExecution.setVariable("isWin", isWin);
     }
 }
